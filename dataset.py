4fb1dda2cf759d69b8d8ccfaee27ea7cf12dd9c1
ShubhamKaushal15 <shubham.a.kaushal@gmail.com>
2022-11-28 03:15:23 -0500

    training ready

---
+++ dataset.py
@@ -0,0 +1,126 @@
+# encoding: utf-8
+import numpy as np
+import cv2
+import os
+from torch.utils.data import Dataset
+import torch
+import glob
+import editdistance
+
+class LipReadSet(Dataset):
+    letters = ' abcdefghijklmnopqrstuvwxyz'
+    start = 1
+    """
+    start: amount by which to offset integer labels. 0 is usually for blank in CTC
+    """
+
+    def __init__(self, video_path, anno_path, file_list, vid_pad = 75, txt_pad = 32, phase = 'train'):
+        """
+        video_path: directory with s{i} folders. video_path/s{i}/abcd1e/0*.png
+        anno_path: directory with s{i} folders. anno_path/s{i}/align/abcd1e.align
+        file_list: path to file. file has paths of the form s{i}/abcd1e
+        txt_pad = len("place ") + len("green ") + len("with ") + len("A ") + len("seven ") + len("please")
+                =  6 + 6 + 5 + 2 + 6 + 7 = 32
+        """
+        self.anno_path = anno_path
+        self.vid_pad = vid_pad
+        self.txt_pad = txt_pad
+        self.phase = phase
+        
+        with open(file_list, 'r') as f:
+            self.video_paths = [os.path.join(video_path, line.strip()) for line in f.readlines()]
+            
+        self.data = []
+        for path in self.video_paths:
+            items = path.split(os.path.sep)            
+            self.data.append((path, items[-2], items[-1]))
+        
+                
+    def __getitem__(self, idx):
+        (path, spk, name) = self.data[idx]
+        vid = self._load_vid(path)
+        anno = self._load_anno(os.path.join(self.anno_path, spk, 'align', name + '.align'))
+
+        # TODO: add data augmentation for training: albumentations
+              
+        vid_len = vid.shape[0]
+        anno_len = anno.shape[0]
+        vid = self._padding(vid, self.vid_pad)
+        anno = self._padding(anno, self.txt_pad)
+        
+        return {'vid': torch.FloatTensor(vid.transpose(3, 0, 1, 2)), 
+            'txt': torch.LongTensor(anno),
+            'txt_len': anno_len,
+            'vid_len': vid_len}
+            
+    def __len__(self):
+        return len(self.data)
+        
+    def _load_vid(self, p): 
+        files = os.listdir(p)
+        files = list(filter(lambda file: file.find('.png') != -1, files))
+        files = sorted(files, key=lambda file: int(os.path.splitext(file)[0]))
+        array = [cv2.imread(os.path.join(p, file)) for file in files]
+        array = list(filter(lambda im: not im is None, array))
+        array = np.stack(array, axis=0).astype(np.float32)
+        return array
+    
+    def _load_anno(self, name):
+        with open(name, 'r') as f:
+            lines = [line.strip().split(' ') for line in f.readlines()]
+            txt = [line[2] for line in lines]
+            txt = list(filter(lambda s: not s.lower() in ['sil', 'sp'], txt))
+        return LipReadSet.txt2arr(' '.join(txt).lower(), 1)
+    
+    def _padding(self, array, length):
+        """
+        padding along sequence axis with zeros to become equal to `length`
+        """
+        array_copy = array.copy()
+        size = array_copy[0].shape
+        for _ in range(length - len(array_copy)):
+            array_copy.append(np.zeros(size))
+        return np.stack(array, axis=0)
+    
+    @staticmethod
+    def txt2arr(txt):
+        arr = [LipReadSet.letters.index(c) for c in txt]
+        
+        return np.array(arr) + LipReadSet.start
+
+    @staticmethod  
+    def arr2txt(arr):
+        txt = []
+        for n in arr:
+            if(n >= LipReadSet.start):
+                txt.append(LipReadSet.letters[n - LipReadSet.start])     
+        return ''.join(txt).strip()
+    
+    @staticmethod
+    def ctc_arr2txt(arr):
+        pre = -1
+        txt = []
+        for n in arr:
+            if(pre != n and n >= LipReadSet.start):                
+                if(len(txt) > 0 and txt[-1] == ' ' and LipReadSet.letters[n - LipReadSet.start] == ' '):
+                    pass
+                else:
+                    txt.append(LipReadSet.letters[n - LipReadSet.start])                
+            pre = n
+        return ''.join(txt).strip()
+
+    @staticmethod
+    def ctc_decode(y):
+        y = y.argmax(-1)
+        return [LipReadSet.ctc_arr2txt(y[_]) for _ in range(y.size(0))]
+            
+    @staticmethod
+    def wer(predict, truth):        
+        word_pairs = [(p[0].split(' '), p[1].split(' ')) for p in zip(predict, truth)]
+        wer = [1.0*editdistance.eval(p[0], p[1])/len(p[1]) for p in word_pairs]
+        return wer
+        
+    @staticmethod
+    def cer(predict, truth):        
+        cer = [1.0*editdistance.eval(p[0], p[1])/len(p[1]) for p in zip(predict, truth)]
+        return cer
---
+++ functions.py
@@ -0,0 +1,163 @@
+import os
+import torch
+import tqdm
+from dataset import LipReadSet
+
+def evaluate(model, loss_function, data_loader, device):
+    """
+    Calculates the loss and error of the model on the data
+    """
+
+    model.eval() # setting model to eval mode
+
+    running_loss = 0.0
+    running_wer = 0
+    running_cer = 0
+
+    for _, data in enumerate(data_loader, 0):
+        
+        # get the inputs; data is a dictionary
+        inputs = data.get('vid').to(device)
+        targets = data.get('txt').to(device)
+        vid_len = data.get('vid_len').to(device)
+        txt_len = data.get('txt_len').to(device)
+
+        # getting predictions
+        outputs = model(inputs)
+        pred_txt = LipReadSet.ctc_decode(outputs)
+
+        # calculate CTC loss
+        loss = loss_function(outputs.transpose(0, 1).log_softmax(-1), targets, vid_len.view(-1), txt_len.view(-1))
+        running_loss += loss.item()
+
+        # getting target text
+        target_txt = [LipReadSet.arr2txt(targets[_]) for _ in range(targets.size(0))]
+
+        running_wer += LipReadSet.wer(pred_txt, target_txt)
+        running_cer += LipReadSet.cer(pred_txt, target_txt)
+
+    final_loss = running_loss / len(data_loader)
+    final_wer = running_wer / len(data_loader)
+    final_cer = running_cer / len(data_loader)
+
+    return final_loss, final_wer, final_cer
+
+def train(model, num_epochs, loss_function, optimizer, model_alias, 
+          train_loader, val_loader, test_loader, device,
+          model_save_dir = os.path.join("models")):
+    """
+    Trains the model using given hyperparameters. Saves the best model based
+    on validation loss.
+    Saves the list of validation losses of the model during training.
+    """
+    model_save_path = os.path.join(model_save_dir, model_alias)
+
+    best_loss = 10000
+    
+    if not os.path.exists(model_save_path):
+      print(f"{model_save_path} path does not exist. Creating...")
+      os.makedirs(f"{model_save_path}")
+
+      with open(os.path.join(model_save_path, "model.info"), 'w') as description_file:
+          description_file.write(f"Model name: {type(model)}\n")
+          description_file.write(f"Optimizer: {type(optimizer)}\n")
+          description_file.write("Best Loss:\n")
+          description_file.write("WER:\n")
+          description_file.write("CER:")
+    else:
+        print("Resuming training from previous best checkpoint...")
+
+        model.to(device)
+        # model.double()
+
+        loaded_checkpoint = torch.load(os.path.join(model_save_path, f"{model_alias}.pth"))
+        model.load_state_dict(loaded_checkpoint['model_state_dict'])
+        optimizer.load_state_dict(loaded_checkpoint['optimizer_state_dict'])
+
+        with open(os.path.join(model_save_path, "model.info"), 'r') as description_file:
+            lines = description_file.readlines()
+            best_loss = float(lines[-1].split(":")[-1].strip())
+
+    validation_loss_file_path = os.path.join(model_save_path, "validation_losses.info")
+    validation_wer_file_path = os.path.join(model_save_path, "validation_wer.info")
+    validation_cer_file_path = os.path.join(model_save_path, "validation_cer.info")
+
+    if not os.path.exists(validation_loss_file_path):
+        with open(validation_loss_file_path, 'w') as a, open(validation_wer_file_path, 'w') as b, open(validation_cer_file_path, 'w') as c:
+            pass
+
+    model.to(device) # get model to current device
+    # model.double()
+
+    for _ in range(num_epochs):  # loop over the dataset multiple times
+
+        model.train() # set model to train mode
+
+        for _, data in enumerate(tqdm(train_loader), 0):
+
+            # get the inputs; data is a dictionary
+            inputs = data.get('vid').to(device)
+            targets = data.get('txt').to(device)
+            vid_len = data.get('vid_len').to(device)
+            txt_len = data.get('txt_len').to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # getting predictions
+            outputs = model(inputs)
+
+            # calculate CTC loss
+            loss = loss_function(outputs.transpose(0, 1).log_softmax(-1), targets, vid_len.view(-1), txt_len.view(-1))
+
+            # backprop
+            loss.backward()
+
+            # update gradients
+            optimizer.step()
+
+        val_loss, val_wer, val_cer = evaluate(model, loss_function, val_loader)
+
+        with open(validation_loss_file_path, 'a') as loss_file:
+            loss_file.write(f"{val_loss}\n")
+
+        with open(validation_wer_file_path, 'a') as loss_file:
+            loss_file.write(f"{val_wer}\n")
+
+        with open(validation_cer_file_path, 'a') as loss_file:
+            loss_file.write(f"{val_cer}\n")
+
+        # Saving best model based on validation loss
+        if val_loss <= best_loss:
+
+            best_loss = val_loss
+
+            save_path = os.path.join(model_save_path, f"{model_alias}.pth")
+            if os.path.exists(save_path):
+                os.remove(save_path)
+
+            checkpoint = {'model': type(model),
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict' : optimizer.state_dict()}
+            torch.save(checkpoint, save_path)
+
+            # Save best loss in the model info file
+            lines = open(os.path.join(model_save_path, "model.info"), 'r').readlines() # !may cause memory issues!
+            lines[-3] = f"Best Loss: {best_loss}\n"
+            lines[-2] = f"WER: {val_wer}\n"
+            lines[-1] = f"CER: {val_cer}"
+            description_file = open(os.path.join(model_save_path, "model.info"), 'w')
+            description_file.writelines(lines)
+            description_file.close()
+
+    loaded_checkpoint = torch.load(os.path.join(model_save_path, f"{model_alias}.pth"))
+    model.load_state_dict(loaded_checkpoint['model_state_dict'])
+    model.to(device)
+
+    with open(os.path.join(model_save_path, "training.success"), 'w') as success_file:
+        test_loss, test_wer, test_cer = evaluate(model, loss_function, test_loader, device)
+        success_file.write(f"Test Loss: {test_loss:.6f}\n")
+        success_file.write(f"Test WER: {test_wer:.6f}\n")
+        success_file.write(f"Test CER: {test_cer:.6f}") 
+
+    return 1
