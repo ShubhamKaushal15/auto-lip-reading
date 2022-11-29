bc4c55b0735b9f9a28491c1fc2c65b4313a83fb4
ShubhamKaushalGCP <jupyter@lipreader-ws.c.concise-dolphin-367018.internal>
2022-11-29 23:18:17 +0000

    pretrained model format converted

--- train.py
+++ train.py
@@ -23,14 +23,14 @@ def main(config_name):
     model = ConvGRU() # Needs to be hard-coded
     model.to(device)
 
-    pretrained_dict = torch.load(config['pretrained_path'])
-    model_dict = model.state_dict()
-    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict.keys() and v.size() == model_dict[k].size()}
-    missed_params = [k for k, v in model_dict.items() if not k in pretrained_dict.keys()]
-    print('loaded params/tot params:{}/{}'.format(len(pretrained_dict),len(model_dict)))
-    print('miss matched params:{}'.format(missed_params))
-    model_dict.update(pretrained_dict)
-    model.load_state_dict(model_dict)
+    # pretrained_dict = torch.load(config['pretrained_path'])
+    # model_dict = model.state_dict()
+    # pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict.keys() and v.size() == model_dict[k].size()}
+    # missed_params = [k for k, v in model_dict.items() if not k in pretrained_dict.keys()]
+    # print('loaded params/tot params:{}/{}'.format(len(pretrained_dict),len(model_dict)))
+    # print('miss matched params:{}'.format(missed_params))
+    # model_dict.update(pretrained_dict)
+    # model.load_state_dict(model_dict)
 
     ctc_loss = nn.CTCLoss()
     adam_optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'], weight_decay=0., amsgrad=True)
